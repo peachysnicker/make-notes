1ec158c11eca5b2910ea8be9adaf2acbd830943e
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-16 20:32:49 -0800

    incomplete but out of time

--- server.js
+++ server.js
@@ -1,111 +1,66 @@
+//Required libraries 
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const notes = require('./db/db.json') || [];
-let id_generator = notes[0] || 0;
-
 const app = express();
-const PORT = process.env.port || 3001;
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
-// /api/notes should read the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      //Parses the data and returns all saved notes
-      let notes = JSON.parse(data);
-      res.json(notes);
-    });
-  });
+const port = process.env.PORT || 3001;
 
-//posting the new note?
-  function writeFileSyncFun(path_=path.join(__dirname, './db/db.json'), data=notes){
-    fs.writeFileSync(path_, JSON.stringify(data, null, 2));
-}
-
-app.post('/api/notes', (req, res) => {
-    let body = req.body;
-    body = id_generator;
-    id_generator ++;
-    notes[0] = id_generator;
-    notes.push(body);
-    writeFileSyncFun()
-    res.json(body);
-    });
-
-// POST the new note?
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request has been received to add your note`);
-
-    const { title, text } = req.body;
-
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-        };
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-    console.log(response);
-
-    res.status(201).json(response);
-
-    } else {
-
-    res.status(500).json('Error in saving your note');
-    }
-});
-
-//retrieving previous saved notes from the db.json database
-app.post('/api/notes', (req, res) => {
-   
-    fs.readFile('./db/db.json', 'utf8', (err, response) => {
-      if (err) throw err;
-      const previousNotes = JSON.parse(response);
-      previousNotes.push({
-        title: req.body.title,
-        text: req.body.text,
-      });
-      fs.writeFile('db/db.json', JSON.stringify(oldNotes, null, 4), (err) => {
-        if (err) throw err;
-        res.json(previousNotes);
-      })
-    });
-  });
-
-// Middleware for parsing JSON and urlencoded form data
+// Middleware for parsing JSON and url-encoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-//create GET * should return the index.html file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'index.html'));
+// GET Route for index.html - file is transfered in response
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-
-//create ROUTE GET /notes should return the notes.html file.
+// GET Route for notes.html - file is transfered as response
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/notes.html'));
+  res.sendFile(__dirname + '/public/notes.html');
 });
 
+//function to read db.json file
+function notesFunction() {
+  const database = fs.readFileSync('./db/db.json', 'utf8');
+  return JSON.parse(database);
+}
 
-//order matters this * route needs to be at the bottom
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+//GET route for api notes and read db.json file to show saved notes
+app.get('/api/notes', (req, res) => {
+    const notes = notesFunction();
+    res.json(notes);
 });
 
-
-//attempt to delete
-
-
-//set up listen for port
-app.listen(PORT, () => {
-    console.log(`Your app is listening at http://localhost:${PORT}`);
+//POST route to save note on req body add db.json
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  let notes = notesFunction() 
+    notes.push({
+      title: req.body.title,
+      text: req.body.text,
+      id: uuid(),
+    });
+    fs.writeFile('db/db.json', JSON.stringify(notes, null, 4), (err) => {
+      if (err) throw err;
+      res.json(notes);
+    })
 });
 
+// DELETE Route for a specific tip
+app.delete('/api/notes/:id', (req, res) => {
+  const noteID = req.params.id;
 
+   let notes = notesFunction()
+   
+    notes = notes.filter((note) => note.id !== noteID);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json(notes);
+    })
+});
+
+//Starts the server
+app.listen(port, () => console.log(`Server started at port ${port}`));
