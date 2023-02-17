98d49bd1221836141dee5978b495e8d3c5832714
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-16 18:02:44 -0800

    posting notes not working yet

--- server.js
+++ server.js
@@ -1,11 +1,65 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const notes_db = require('./db/db.json');
+const notes = require('./db/db.json') || [];
 
 const app = express();
 const PORT = process.env.port || 3001;
 
+// /api/notes should read the db.json file and return all saved notes as JSON
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      //Parses the data and returns all saved notes
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+
+// POST the new note
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request has been received to add your note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+    console.log(response);
+
+    res.status(201).json(response);
+
+    } else {
+
+    res.status(500).json('Error in saving your note');
+    }
+});
+
+//retrieving previous saved notes from the db.json database
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    fs.readFile('./db/db.json', 'utf8', (err, response) => {
+      if (err) throw err;
+      const previousNotes = JSON.parse(response);
+      previousNotes.push({
+        title: req.body.title,
+        text: req.body.text,
+        id: id++,
+      });
+      fs.writeFile('db/db.json', JSON.stringify(oldNotes, null, 4), (err) => {
+        if (err) throw err;
+        res.json(previousNotes);
+      })
+    });
+  });
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -13,14 +67,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-//create GET * should return the index.html file.
-// '/' is my root but must change to *
+//create GET * should return the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + 'index.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
@@ -29,6 +78,14 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
 
+
+//order matters this * route needs to be at the bottom
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+//attempt to delete
 
 
 //set up listen for port
